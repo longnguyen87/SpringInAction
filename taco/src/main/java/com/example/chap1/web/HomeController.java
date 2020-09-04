036ae094a7c6579caa84808b32957cad4fcde4ad
Long Nguyen <nguyenhlongvn@gmail.com>
2020-09-04 10:05:07 -0500

    add all

---
+++ taco/src/main/java/com/example/chap1/web/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.chap1.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
